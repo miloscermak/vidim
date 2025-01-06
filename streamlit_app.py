0c69457fb234f09aa8541ca7b6284ee4223f05b6
miloscermak <130353544+miloscermak@users.noreply.github.com>
2025-01-06 21:53:58 +0100

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -126,7 +126,7 @@ if uploaded_file is not None:
         
         # Zobrazení náhledu
         image = Image.open(io.BytesIO(image_data))
-        st.image(image, caption="Náhled obrázku", use_column_width=True)
+        st.image(image, caption="Náhled obrázku", use_container_width=True)
         
         # Získání metadat
         image_bytes = io.BytesIO(uploaded_file.getvalue())
@@ -156,7 +156,32 @@ if uploaded_file is not None:
             with st.spinner('Probíhá analýza...'):
                 base64_image = base64.b64encode(image_data).decode("utf-8")
                 
-                prompt = """Prohlédni si pozorně následující fotografii: <image> {{IMAGE}} </image> Tvým úkolem je identifikovat nejzajímavější nebo nejpozoruhodnější prvek na této fotografii. Může to být osoba, předmět, událost, detail nebo cokoliv jiného, co tě zaujme jako nejvíce pozoruhodné. Poté napiš krátký popisek tohoto prvku. Popisek by měl mít následující vlastnosti: - Délka přibližně tři až čtyři věty - Zaměření na to, co činí tento prvek tak zajímavým nebo pozoruhodným - Poskytnutí relevantních detailů, které podporují tvůj výběr jako nejzajímavějšího prvku Pamatuj, že cílem je najít a popsat to opravdu nejpozoruhodnější na celé fotografii. Nesnaž se popsat celou scénu, ale soustřeď se na ten jeden aspekt, který nejvíce vyniká svou zajímavostí nebo neobvyklostí. Svůj popisek napiš v českém jazyce. Začni zajímavě a novinářsky, ne frází typu :"Nejzajímavějším prvkem na fotce ..." a podobně. Začni čtiově, klidně i provokativně. Piš elegantně a chytře.
+                prompt = """Prohlédni si pozorně následující fotografii jídla:
+
+                <image>
+                {{IMAGE}}
+                </image>
+
+                Pečlivě si prohlédni všechny detaily zobrazené na fotografii. Zaměř se na ingredience, způsob přípravy, velikost porce a celkový vzhled jídla.
+
+                Na základě svého pozorování proveď následující úkoly:
+
+                1. Navrhni vhodný název pro toto jídlo v češtině. Název by měl být výstižný a popisný.
+
+                2. Odhadni přibližnou kalorickou hodnotu zobrazeného jídla. Vezmi v úvahu viditelné ingredience, velikost porce a předpokládaný způsob přípravy.
+
+                3. Promysli si dané jídlo a napiš o něm základní informaci.
+
+                Svou odpověď napiš v následujícím formátu:
+
+                Název jídla:
+                [Zde uveď navržený název jídla v češtině]
+
+                Kalorická hodnota:
+                [Zde uveď odhadovanou kalorickou hodnotu jídla v češtině, včetně zdůvodnění svého odhadu]
+                
+                Poznámky:
+                [Zde napiš vše, co o jídle víš]
                 """
 
                 try:
